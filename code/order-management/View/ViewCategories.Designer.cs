8d061ecaace5034ce99ae2a5083689d1de806406
Ricardo Coelho <devel@rmorgado.ch>
2021-01-17 15:10:35 +0100

    view/ViewCategories: make layout resonsive

--- code/order-management/View/ViewCategories.Designer.cs
+++ code/order-management/View/ViewCategories.Designer.cs
@@ -34,29 +34,38 @@
             this.CmdDelete = new System.Windows.Forms.Button();
             this.TxtSearch = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.DgvCategories)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // DgvCategories
             // 
             this.DgvCategories.AllowUserToAddRows = false;
             this.DgvCategories.AllowUserToDeleteRows = false;
+            this.DgvCategories.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.DgvCategories.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DgvCategories.Location = new System.Drawing.Point(57, 176);
+            this.DgvCategories.Location = new System.Drawing.Point(3, 127);
             this.DgvCategories.Name = "DgvCategories";
             this.DgvCategories.ReadOnly = true;
             this.DgvCategories.RowHeadersWidth = 92;
             this.DgvCategories.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.DgvCategories.Size = new System.Drawing.Size(1140, 449);
+            this.DgvCategories.Size = new System.Drawing.Size(1060, 631);
             this.DgvCategories.TabIndex = 5;
             this.DgvCategories.Text = "dataGridView1";
             this.DgvCategories.SelectionChanged += new System.EventHandler(this.DgvCategories_SelectionChanged);
             // 
             // CmdAddNew
             // 
-            this.CmdAddNew.Location = new System.Drawing.Point(585, 661);
+            this.CmdAddNew.Location = new System.Drawing.Point(3, 3);
             this.CmdAddNew.Name = "CmdAddNew";
-            this.CmdAddNew.Size = new System.Drawing.Size(185, 79);
+            this.CmdAddNew.Size = new System.Drawing.Size(160, 68);
             this.CmdAddNew.TabIndex = 1;
             this.CmdAddNew.Text = "Add New";
             this.CmdAddNew.UseVisualStyleBackColor = true;
@@ -65,9 +74,9 @@
             // CmdEdit
             // 
             this.CmdEdit.Enabled = false;
-            this.CmdEdit.Location = new System.Drawing.Point(800, 661);
+            this.CmdEdit.Location = new System.Drawing.Point(169, 3);
             this.CmdEdit.Name = "CmdEdit";
-            this.CmdEdit.Size = new System.Drawing.Size(185, 79);
+            this.CmdEdit.Size = new System.Drawing.Size(160, 68);
             this.CmdEdit.TabIndex = 1;
             this.CmdEdit.Text = "Edit";
             this.CmdEdit.UseVisualStyleBackColor = true;
@@ -76,9 +85,9 @@
             // CmdDelete
             // 
             this.CmdDelete.Enabled = false;
-            this.CmdDelete.Location = new System.Drawing.Point(1012, 661);
+            this.CmdDelete.Location = new System.Drawing.Point(335, 3);
             this.CmdDelete.Name = "CmdDelete";
-            this.CmdDelete.Size = new System.Drawing.Size(185, 79);
+            this.CmdDelete.Size = new System.Drawing.Size(160, 68);
             this.CmdDelete.TabIndex = 1;
             this.CmdDelete.Text = "Delete";
             this.CmdDelete.UseVisualStyleBackColor = true;
@@ -86,37 +95,79 @@
             // 
             // TxtSearch
             // 
-            this.TxtSearch.Location = new System.Drawing.Point(57, 109);
+            this.TxtSearch.Location = new System.Drawing.Point(10, 44);
             this.TxtSearch.Name = "TxtSearch";
-            this.TxtSearch.Size = new System.Drawing.Size(1140, 43);
+            this.TxtSearch.Size = new System.Drawing.Size(626, 39);
             this.TxtSearch.TabIndex = 2;
             this.TxtSearch.TextChanged += new System.EventHandler(this.TxtSearch_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(57, 55);
+            this.label1.Location = new System.Drawing.Point(10, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 37);
+            this.label1.Size = new System.Drawing.Size(85, 32);
             this.label1.TabIndex = 3;
             this.label1.Text = "Search";
             // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.TxtSearch);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1060, 118);
+            this.panel1.TabIndex = 6;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.DgvCategories, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.32911F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.67088F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1066, 887);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.CmdAddNew);
+            this.flowLayoutPanel1.Controls.Add(this.CmdEdit);
+            this.flowLayoutPanel1.Controls.Add(this.CmdDelete);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 764);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1060, 120);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
             // ViewCategories
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1255, 795);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.TxtSearch);
-            this.Controls.Add(this.CmdDelete);
-            this.Controls.Add(this.CmdEdit);
-            this.Controls.Add(this.CmdAddNew);
-            this.Controls.Add(this.DgvCategories);
+            this.ClientSize = new System.Drawing.Size(1090, 923);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "ViewCategories";
             this.Text = "ViewCategories";
             ((System.ComponentModel.ISupportInitialize)(this.DgvCategories)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -128,5 +179,8 @@
         private System.Windows.Forms.Button CmdDelete;
         private System.Windows.Forms.TextBox TxtSearch;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
