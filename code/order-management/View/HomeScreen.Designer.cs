a7bc693643fe7676f5b3a85f77c422fea7bb2441
Ricardo Coelho <devel@rmorgado.ch>
2021-01-17 14:48:20 +0100

    view/HomeScreen: add flowLayout

--- code/order-management/View/HomeScreen.Designer.cs
+++ code/order-management/View/HomeScreen.Designer.cs
@@ -29,45 +29,30 @@
         private void InitializeComponent()
         {
             this.CmdCustomers = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.CmdCategoryTreeView = new System.Windows.Forms.Button();
             this.CmdYoyComparison = new System.Windows.Forms.Button();
             this.CmdOrders = new System.Windows.Forms.Button();
             this.CmdProducts = new System.Windows.Forms.Button();
             this.CmdCategories = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // CmdCustomers
             // 
-            this.CmdCustomers.Location = new System.Drawing.Point(74, 74);
+            this.CmdCustomers.Location = new System.Drawing.Point(3, 3);
             this.CmdCustomers.Name = "CmdCustomers";
-            this.CmdCustomers.Size = new System.Drawing.Size(204, 105);
+            this.CmdCustomers.Size = new System.Drawing.Size(177, 91);
             this.CmdCustomers.TabIndex = 0;
             this.CmdCustomers.Text = "Customers";
             this.CmdCustomers.UseVisualStyleBackColor = true;
             this.CmdCustomers.Click += new System.EventHandler(this.CmdCustomers_Click);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.CmdCategoryTreeView);
-            this.groupBox1.Controls.Add(this.CmdYoyComparison);
-            this.groupBox1.Controls.Add(this.CmdOrders);
-            this.groupBox1.Controls.Add(this.CmdProducts);
-            this.groupBox1.Controls.Add(this.CmdCategories);
-            this.groupBox1.Controls.Add(this.CmdCustomers);
-            this.groupBox1.Location = new System.Drawing.Point(36, 29);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1094, 383);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.UseCompatibleTextRendering = true;
-            // 
             // CmdCategoryTreeView
             // 
-            this.CmdCategoryTreeView.Location = new System.Drawing.Point(314, 212);
+            this.CmdCategoryTreeView.Location = new System.Drawing.Point(735, 3);
             this.CmdCategoryTreeView.Name = "CmdCategoryTreeView";
-            this.CmdCategoryTreeView.Size = new System.Drawing.Size(204, 105);
+            this.CmdCategoryTreeView.Size = new System.Drawing.Size(177, 91);
             this.CmdCategoryTreeView.TabIndex = 0;
             this.CmdCategoryTreeView.Text = "Category Treeview";
             this.CmdCategoryTreeView.UseVisualStyleBackColor = true;
@@ -75,18 +60,18 @@
             // 
             // CmdYoyComparison
             // 
-            this.CmdYoyComparison.Location = new System.Drawing.Point(557, 212);
+            this.CmdYoyComparison.Location = new System.Drawing.Point(3, 100);
             this.CmdYoyComparison.Name = "CmdYoyComparison";
-            this.CmdYoyComparison.Size = new System.Drawing.Size(204, 105);
+            this.CmdYoyComparison.Size = new System.Drawing.Size(177, 91);
             this.CmdYoyComparison.TabIndex = 0;
             this.CmdYoyComparison.Text = "YOY Comparison";
             this.CmdYoyComparison.UseVisualStyleBackColor = true;
             // 
             // CmdOrders
             // 
-            this.CmdOrders.Location = new System.Drawing.Point(807, 74);
+            this.CmdOrders.Location = new System.Drawing.Point(552, 3);
             this.CmdOrders.Name = "CmdOrders";
-            this.CmdOrders.Size = new System.Drawing.Size(204, 105);
+            this.CmdOrders.Size = new System.Drawing.Size(177, 91);
             this.CmdOrders.TabIndex = 0;
             this.CmdOrders.Text = "Orders";
             this.CmdOrders.UseVisualStyleBackColor = true;
@@ -94,9 +79,9 @@
             // 
             // CmdProducts
             // 
-            this.CmdProducts.Location = new System.Drawing.Point(557, 74);
+            this.CmdProducts.Location = new System.Drawing.Point(369, 3);
             this.CmdProducts.Name = "CmdProducts";
-            this.CmdProducts.Size = new System.Drawing.Size(204, 105);
+            this.CmdProducts.Size = new System.Drawing.Size(177, 91);
             this.CmdProducts.TabIndex = 0;
             this.CmdProducts.Text = "Products";
             this.CmdProducts.UseVisualStyleBackColor = true;
@@ -104,23 +89,39 @@
             // 
             // CmdCategories
             // 
-            this.CmdCategories.Location = new System.Drawing.Point(314, 74);
+            this.CmdCategories.Location = new System.Drawing.Point(186, 3);
             this.CmdCategories.Name = "CmdCategories";
-            this.CmdCategories.Size = new System.Drawing.Size(204, 105);
+            this.CmdCategories.Size = new System.Drawing.Size(177, 91);
             this.CmdCategories.TabIndex = 0;
             this.CmdCategories.Text = "Categories";
             this.CmdCategories.UseVisualStyleBackColor = true;
             this.CmdCategories.Click += new System.EventHandler(this.CmdCategories_Click);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.CmdCustomers);
+            this.flowLayoutPanel1.Controls.Add(this.CmdCategories);
+            this.flowLayoutPanel1.Controls.Add(this.CmdProducts);
+            this.flowLayoutPanel1.Controls.Add(this.CmdOrders);
+            this.flowLayoutPanel1.Controls.Add(this.CmdCategoryTreeView);
+            this.flowLayoutPanel1.Controls.Add(this.CmdYoyComparison);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(23, 30);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1094, 546);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
             // HomeScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1183, 673);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(1129, 609);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "HomeScreen";
             this.Text = "Home";
-            this.groupBox1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -128,11 +129,11 @@
         #endregion
 
         private System.Windows.Forms.Button CmdCustomers;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button CmdOrders;
         private System.Windows.Forms.Button CmdProducts;
         private System.Windows.Forms.Button CmdCategories;
         private System.Windows.Forms.Button CmdYoyComparison;
         private System.Windows.Forms.Button CmdCategoryTreeView;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
