57aa5babcad035d91c7a405e5caf877480ea370e
leafar396 <raphi.wirth@gmail.com>
2021-08-18 20:16:13 +0200

    add unittest project

---
+++ code/order-management.tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace order_management.tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ code/order-management.tests/dbModels/CustomerTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace order_management.tests.dbModels
+{
+    [TestFixture]
+    class CustomerTest
+    {
+        [Test]
+        public void Constructor()
+        {
+            Assert.That(5, Is.EqualTo(5));
+        }
+    }
+}
