88719142a0aa545b0b29ed137d670639757d0800
leafar396 <raphi.wirth@gmail.com>
2021-08-25 18:14:16 +0200

    use inmemory db for repo tests

---
+++ code/order-management.tests/Repos/FakeContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace order_management.tests.Repos
+{
+    public class FakeContext : OrderManagementDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "in_memory_test_db");
+        }
+    }
+}
---
+++ code/order-management.tests/Repos/RepoCustomerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace order_management.tests.Repos
+{
+    public class RepoCustomerTests
+    {
+        [Fact]
+        public void GetAllReturnsValidEntity()
+        {
+            var fakeContext = new FakeContext();
+            var repo = new RepoCustomer(fakeContext);
+
+            var retrieved = (List<Customer>)repo.GetAll();
+
+            Assert.Equal("Hans", retrieved[0].FirstName);
+        }
+
+        //[Fact]
+        //public void GetByNameReturnsValidEntity()
+        //{
+        //    var fakeContext = new FakeContext();
+        //    var repo = new RepoCustomer(fakeContext);
+
+        //    var retrieved = repo.GetByName("Hans", "Müller");
+
+        //    Assert.Equal(1000, retrieved.CustomerId);
+        //}
+    }
+}
