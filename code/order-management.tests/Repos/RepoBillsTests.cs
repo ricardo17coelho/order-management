d18b0934b6fe7eea9470f0b8992849f5e1b2f4f3
leafar396 <raphi.wirth@gmail.com>
2021-08-31 18:25:25 +0200

    more tests...

---
+++ code/order-management.tests/Repos/RepoBillsTests.cs
@@ -0,0 +1,41 @@
+﻿using FakeItEasy;
+using order_management;
+using order_management.Services;
+using order_management.tests.Repos;
+using System.Collections.Generic;
+using Xunit;
+
+namespace bill_management.tests.Repos
+{
+    public class RepoBillTests
+    {
+        [Fact]
+        public void GetAllReturnsValidEntity()
+        {
+            var fakeContext = new FakeContext();
+            var fakeCustomerService = A.Fake<CustomerService>();
+            var fakeOrderDetailService = A.Fake<OrderDetailService>();
+
+            var repo = new RepoBill(fakeContext, fakeCustomerService, fakeOrderDetailService);
+
+            var retrieved = (List<Bill>)repo.GetAll();
+
+            Assert.Equal(1000, retrieved[0].BillId);
+        }
+
+        [Fact]
+        public void SearchReturnsValidEntity()
+        {
+            var fakeContext = new FakeContext();
+            var fakeCustomerService = A.Fake<CustomerService>();
+            var fakeOrderDetailService = A.Fake<OrderDetailService>();
+
+            var repo = new RepoBill(fakeContext, fakeCustomerService, fakeOrderDetailService);
+
+            var retrieved = repo.Search("Hans");
+
+            Assert.Equal(1000, retrieved[0].BillId);
+        }
+
+    }
+}
---
+++ code/order-management.tests/Repos/RepoYoyComparisonTests.cs
@@ -0,0 +1,21 @@
+﻿using order_management;
+using order_management.tests.Repos;
+using System.Collections.Generic;
+using Xunit;
+
+namespace yoyComparison_management.tests.Repos
+{
+    public class RepoYoyComparisonTests
+    {
+        //[Fact]
+        //public void GetYoyComparisonReturnsValidEntity()
+        //{
+        //    var fakeContext = new FakeContext();
+        //    var repo = new RepoYoyComparison(fakeContext);
+
+        //    var retrieved = (List<YoyComparison>)repo.GetYoyComparison();
+
+        //    Assert.Equal(5, retrieved[0].CountOrders);
+        //}
+    }
+}
